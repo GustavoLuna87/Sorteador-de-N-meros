79ff00b046568c1209d65f96376dccf3cf7958c9
GustavoLuna87 <gustavo.luna87@hotmail.com>
2024-04-05 14:12:56 -0300

    Atualização no JS

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ button.addEventListener('click', function() {
     
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+
+    if( min >= max ){
+      alert( "O valor mínimo tem que ser Menor que o valor máximo")
+    }
   
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
